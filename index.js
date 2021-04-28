72c951995ae790c6ac0f2b244eda63d4c939adc3
Curtis <c.b.punches@gmail.com>
2021-04-28 07:55:57 -0400

    test done

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ function translate(string){
   // console.log(string);
   string = string.toLowerCase();
  let vowels = ["a","i","e","o","u"];
- if (string[0].match(vowels)){
+ firstVowel = 0;
+ if (vowels.includes(string[0])){
    return string + "way";
- }
-
-
+ }else
+for (let letters of string){
+  if(vowels.includes(letters)){
+    firstVowel=string.indexOf(letters);
+    break;
+  }
 }
+return string.slice(firstVowel) + string.slice(0,firstVowel) + "ay"
+}
+
+module.exports.translate = translate;
--- index.test.js
+++ index.test.js
@@ -5,6 +5,50 @@ describe("translate", () => {
     let result = translate("Apple");
     expect(result).toEqual("appleway");
   });
-
+  test ("banana string", () => {
+    let result = translate("earth");
+    expect(result).toEqual("earthway");
+  });
+  
+  test ("banana string", () => {
+    let result = translate("Ice");
+    expect(result).toEqual("iceway");
+  });
+  test ("banana string", () => {
+    let result = translate("ocean");
+    expect(result).toEqual("oceanway");
+  });
+  test ("banana string", () => {
+    let result = translate("underware");
+    expect(result).toEqual("underwareway");
+  });
+  
+  
+  test ("barn string", () => {
+    let result = translate("barn");
+    expect(result).toEqual("arnbay");
+  });
+  
+  test ("banana string", () => {
+    let result = translate("banana");
+    expect(result).toEqual("ananabay");
+  });
+  
+  test ("banana string", () => {
+    let result = translate("hhhh");
+    expect(result).toEqual("hhhhay");
+  });
+  
+  test ("banana string", () => {
+    let result = translate("scholar");
+    expect(result).toEqual("olarschay");
+  });
+  
+  test ("banana string", () => {
+    let result = translate("tractor");
+    expect(result).toEqual("actortray");
+  });
 
 });
+
+
